32060d048c1ea0049e90927600324f52613f57ca
Harmandeep kaur <harmank8683@gmail.com>
2026-02-14 22:14:17 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,12 @@
+# print function to print on the console
+print("hi this is a print function to print something on the console")
+
+name = "Harman" # a variable
+age = 12
+
+# formatting 
+print(f"my name is {name} and i am {age} years old")
+
+# type of the data type stored in the variable
+print(type(name))   # <class 'str'>
+print(type(age))    # <class 'int'>
