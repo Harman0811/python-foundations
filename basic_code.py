dd489a9677bbcdd1ca7971f2b5ad6f6a54d13581
Harmandeep kaur <harmank8683@gmail.com>
2026-02-23 21:16:43 +0530

    code examples in basic_code.py

--- basic_code.py
+++ basic_code.py
@@ -1,18 +1,17 @@
 # Take a number from user and print whether it is:
-# num = int(input("enter a number: "))
-# print("entered number is : ", num, "which is of type", type(num))
-
+num = int(input("enter a number: "))
+print("entered number is : ", num, "which is of type", type(num))
 
 # Swap two numbers without using third variable.
-# a = 12
-# b = 54
+a = 12
+b = 54
 
-# print(f"before swaping: a : {a}  b : {b}")
+print(f"before swaping: a : {a}  b : {b}")
 
-# temp = a
-# a = b
-# b = temp
-# print(f"after swaping : a : {a} b : {b}")
+temp = a
+a = b
+b = temp
+print(f"after swaping : a : {a} b : {b}")
 
 # Print sum of first 100 natural numbers.
 sum = 0
@@ -29,7 +28,6 @@ reverse = num[::-1]
 print("Reversed number:", reverse)
 
 
-
 # Count how many vowels are in a string.
 text = "hey I am coding in VS code and this program is used to count the no. of vowels in a string"
 count = 0
@@ -39,3 +37,4 @@ for ch in text.lower():
         count += 1
 
 # print("Total number of vowels in the given string is:", count)
+
